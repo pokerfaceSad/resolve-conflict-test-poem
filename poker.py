060f8ab06060a3f1cfa55dcc7eea84c1934be508
pokerfaceSad <pokerfacesad@126.com>
2020-03-11 18:26:13 +0800

    完成判断手牌模式功能，通过单元测试

--- poker.py
+++ poker.py
@@ -1,11 +1,71 @@
 import unittest
 
+def get_mode(pokers_str):
+    pokers_list = pokers_str.split(" ")
+    
+    pokers_nums_list = [ poker[0] for poker in pokers_list ]
+    replace_dict = {'J':'11', 'Q':'12', 'K':'13', 'A':'1', 'T':'10'}
+    pokers_nums_list = [ replace_dict[poker_num] if poker_num in replace_dict else poker_num for poker_num in pokers_nums_list ]
+    pokers_nums_list = [int(poker_num) for poker_num in pokers_nums_list]
+    
+    pokers_suits_list = [ poker[1] for poker in pokers_list ]
+    
+    len_pokers_nums_set = len(set(pokers_nums_list))
+    len_pokers_suits_set = len(set(pokers_suits_list))
+    
+    # print("pokers_nums_list:")
+    # print(pokers_nums_list)
+    # print("pokers_suits_list")
+    # print(pokers_suits_list)
+    # print("pokers_nums_set")
+    # print(set(pokers_nums_list))
+    # print("pokers_nums_set")
+    # print(set(pokers_suits_list))
+
+    # 同花顺 9
+    if len_pokers_suits_set == 1 and \
+       len_pokers_nums_set == len(pokers_nums_list) and \
+       max(pokers_nums_list) - min(pokers_nums_list) == len(pokers_nums_list) - 1:
+        return 9
+    
+    # 铁枝 8
+    if max([pokers_nums_list.count(poker_num) for poker_num in pokers_nums_list]) == 4:
+        return 8
+    
+    # 葫芦 7
+    if len_pokers_nums_set == 2 and max([pokers_nums_list.count(poker_num) for poker_num in pokers_nums_list]) == 3:
+        return 7
+    
+    # 同花 6
+    if len_pokers_suits_set == 1:
+        return 6
+    
+    # 顺子 5
+    if len_pokers_nums_set == len(pokers_nums_list) and \
+       max(pokers_nums_list) - min(pokers_nums_list) == len(pokers_nums_list) - 1:
+        return 5
+
+    # 三条 4
+    if max([pokers_nums_list.count(poker_num) for poker_num in pokers_nums_list]) == 3:
+        return 4
+    
+    # 两对 3
+    if len_pokers_nums_set == 3 and max([pokers_nums_list.count(poker_num) for poker_num in pokers_nums_list]) == 2:
+        return 3
+    
+    # 对子 2
+    if max([pokers_nums_list.count(poker_num) for poker_num in pokers_nums_list]) == 2:
+        return 2
+    
+    # 散牌
+    return 1
+
 class poker_test(unittest.TestCase):
     def setUp(self):
         print("Test Start...")
     
     def tearDown(self):
-        print("Teast End...")
+        print("Test End...")
 
     # 判断手牌的模式
     def test_get_mode_1(self):
@@ -33,27 +93,28 @@ class poker_test(unittest.TestCase):
         self.assertEqual(get_mode("2H 3D 4S 5C 6D"), 5)
     
     # 判断手牌的模式
-    def test_get_mode_4(self):
+    def test_get_mode_6(self):
         # 同花 6
         self.assertEqual(get_mode("2H 8H 4H 5H 6H"), 6)
     
     # 判断手牌的模式
-    def test_get_mode_4(self):
+    def test_get_mode_7(self):
         # 葫芦 7
         self.assertEqual(get_mode("2H 2D 2S 3C 3D"), 7)
     
     # 判断手牌的模式
-    def test_get_mode_4(self):
+    def test_get_mode_8(self):
         # 铁枝 8
         self.assertEqual(get_mode("2H 2D 2S 2C 3D"), 8)
     
     # 判断手牌的模式
-    def test_get_mode_4(self):
+    def test_get_mode_9(self):
         # 同花顺 9
         self.assertEqual(get_mode("2D 3D 4D 5D 6D"), 9)
     
     
-    
+if __name__ == "__main__":
+    unittest.main(verbosity=3)
     
     
     
